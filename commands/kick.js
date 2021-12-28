b2b6e6f51d4b1f596db2621a0896fc5c412a08bd
EzHawkeye <66911686+EzHawkeye@users.noreply.github.com>
2021-12-28 21:37:35 +0100

    fff

--- commands/kick.js
+++ commands/kick.js
@@ -12,12 +12,10 @@ module.exports = {
         if (!reason) reason = "No reason given";
 
         const kickembed = new Discord.MessageEmbed()
-        .setColor("RED")
+        .setColor("ORANGE")
         .setTitle (`You were kicked from **${message.guild.name}**`)
         .setDescription(`**Kicked user:** ${mentionMember}
-        
             **Kicked by:** ${message.author}
-
             **Reason:** ${reason}`)
         .setTimestamp()
         .setFooter(bot.user.tag, bot.user.displayAvatarURL())
